1738e085f897e4fbe1f3aacaa203de6372a20a6e
SibahleD <s.dlamini2@alustudent.com>
2025-01-11 20:40:22 +0200

    git2

--- models/base_model.py
+++ models/base_model.py
@@ -22,12 +22,12 @@ class BaseModel:
                         value = datetime.fromisoformat(value)
                     setattr(self, key, value)
         else:
-            self.my_number = str(uuid.uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.my_number}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
