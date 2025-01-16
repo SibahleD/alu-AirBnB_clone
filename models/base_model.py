e7d61ac557ff4aa8ad7d045a6cc8fc2b0e113107
SibahleD <s.dlamini2@alustudent.com>
2025-01-16 12:56:52 +0200

    rse

--- models/base_model.py
+++ models/base_model.py
@@ -16,9 +16,17 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}), {}".format(self.__class__.__name__, self.id, self.__dict__) 
@@ -28,7 +36,7 @@ class BaseModel:
 
     def to_dict(self):
         dict_rep = self.__dict__.copy()
-        dict_rep['__class__'] = self.__class__.__name__
-        dict_rep['created_at'] = self.created_at.isoformat()
-        dict_rep['updated_at'] = self.updated_at.isoformat()
+        dict_rep["created_at"] = self.created_at.isoformat()
+        dict_rep["updated_at"] = self.updated_at.isoformat()
+        dict_rep["__class__"] = self.__class__.__name__
         return dict_rep
