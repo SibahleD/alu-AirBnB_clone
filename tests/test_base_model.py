b87f7609eae0e680c10204ec4c1843fce21fec0f
SibahleD <s.dlamini2@alustudent.com>
2025-01-16 13:07:00 +0200

    testcase

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,34 @@
 #!/usr/bin/python3
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBaseModel(unittest.TestCase):
+    def test_instance(self):
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel)
+        self.assertIsInstance(my_model.id, str)
+        self.assertIsInstance(my_model.created_at, object)
+        self.assertIsInstance(my_model.updated_at, object)
+
+    def test_str(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        self.assertEqual(str(my_model), "[BaseModel] ({}) {}".format(my_model.id, my_model.__dict__))
+
+    def test_save(self):
+        my_model = BaseModel()
+        old_updated_at = my_model.updated_at
+        my_model.save()
+        self.assertNotEqual(old_updated_at, my_model.updated_at)
+
+    def test_to_dict(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        self.assertIsInstance(my_model_json, dict)
+        self.assertIsInstance(my_model_json["created_at"], str)
+        self.assertIsInstance(my_model_json["updated_at"], str)
+        self.assertIsInstance(my_model_json["__class__"], str)
+        self.assertEqual(my_model_json["__class__"], "BaseModel")
