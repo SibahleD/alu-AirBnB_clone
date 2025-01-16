a6bada8f69ba8a63fa5fb9074f60bb92d7db8266
SibahleD <s.dlamini2@alustudent.com>
2025-01-16 12:31:20 +0200

    Q3test1

--- models/base_model.py
+++ models/base_model.py
@@ -13,18 +13,12 @@ Methods:
 """
 import uuid
 from datetime import datetime
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        value = datetime.fromisoformat(value)
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
         return (
@@ -36,9 +30,8 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        dict_repr = self.__dict__.copy()
-        dict_repr['__class__'] = self.__class__.__name__
-        dict_repr['created_at'] = self.created_at.isoformat()
-        dict_repr['updated_at'] = self.updated_at.isoformat()
-        return dict_repr
-    
+        dict_rep = self.__dict__.copy()
+        dict_rep['__class__'] = self.__class__.__name__
+        dict_rep['created_at'] = self.created_at.isoformat()
+        dict_rep['updated_at'] = self.updated_at.isoformat()
+        return dict_rep
