a6f35ae69b4974b28bc143f7da2fe2af2dfb2f15
SibahleD <s.dlamini2@alustudent.com>
2025-01-17 11:03:49 +0200

    final push

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,79 @@
 #!/usr/bin/python3
 """
-This class defines all common attributes/methods for other classes.
-Attributes:
-    id (str): Unique identifier for each instance.
-    created_at (datetime): The datetime when the instance was created.
-    updated_at (datetime): The datetime when the instance was last updated.
-Methods:
-    __init__(): Initializes a new instance of BaseModel.
-    __str__(): Returns a string representation of the instance.
-    save(): Updates the 'updated_at' attribute with the current datetime.
-    to_dict(): Returns a dictionary containing all keys/values of the instance.
+Custom base class for the entire project
 """
-import uuid
+
+from uuid import uuid4
 from datetime import datetime
-from models import FileStorage
+import models
+
 
 class BaseModel:
+    """Custom base for all the classes in the AirBnb console project
+
+    Arttributes:
+        id(str): handles unique user identity
+        created_at: assigns current datetime
+        updated_at: updates current datetime
+
+    Methods:
+        __str__: prints the class name, id, and creates dictionary
+        representations of the input values
+        save(self): updates instance arttributes with current datetime
+        to_dict(self): returns the dictionary values of the instance obj
+
+    """
+
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key in ["created_at", "updated_at"]:
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
+        """Public instance artributes initialization
+        after creation
+
+        Args:
+            *args(args): arguments
+            **kwargs(dict): attrubute values
+
+        """
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            FileStorage.new(self)
+            for key, value in kwargs.items():
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(
+                        value, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}), {self.__dict__}" 
+        """
+        Returns string representation of the class
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
-        FileStorage.save()
+        """
+        Updates the public instance attribute:
+        'updated_at' - with the current datetime
+        """
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
-        dict_rep = self.__dict__.copy()
-        dict_rep["created_at"] = self.created_at.isoformat()
-        dict_rep["updated_at"] = self.updated_at.isoformat()
-        dict_rep["__class__"] = self.__class__.__name__
-        return dict_rep
+        """
+        Method returns a dictionary containing all
+        keys/values of __dict__ instance
+        """
+        map_objects = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                map_objects[key] = value.isoformat()
+            else:
+                map_objects[key] = value
+        map_objects["__class__"] = self.__class__.__name__
+        return map_objects
--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
-"""Initializes the package"""
+#!/bin/usr/python3
+"""
+Init for models module
+"""
 from models.engine.file_storage import FileStorage
+
 storage = FileStorage()
 storage.reload()
